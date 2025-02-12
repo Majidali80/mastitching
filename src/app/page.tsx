6f4563d232940d80f8b74ea6ae2bd2731b52ec5b
Majid Ali <160537411+Majidali80@users.noreply.github.com>
2025-02-12 16:52:28 +0500

    Update page.tsx
    commit1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
-import Navbar from "@/components/Navbar";
+
 import Products from "@/components/Products";
-import Image from "next/image";
+
 
 export default function Home() {
   return (
     <div>
-      {/* <Navbar /> */}
+
       <Products />
     </div>
   );

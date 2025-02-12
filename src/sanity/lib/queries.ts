7c37d7b7885c297ac85320fa9db2456c1fa7d964
Majid Ali <160537411+Majidali80@users.noreply.github.com>
2025-02-12 17:06:05 +0500

    Update queries.ts

--- src/sanity/lib/queries.ts
+++ src/sanity/lib/queries.ts
@@ -1,27 +1,8 @@
-
-// const query = `*[_type == "product"]`;
-// Fetch All Products
-
-// const query = `*[_type == "product"]`;
-// // Fetch Single Product by ID
-
-// // const query = `*[_type == "product" && _id == $id][0]`;
-// Fetch Products by Category
-
-// const query = `*[_type == "product" && category == $category]`;
-// Fetch Products with Size Options and Prices
-import { defineQuery } from "next-sanity";
 import { groq } from "next-sanity";
-// sanity/queries.js
 
-export const Query = `*[_type == "product"]`;
-export const productByIdQuery = `*[_type == "product" && _id == $id][0]`;
-export const productsByCategoryQuery = `*[_type == "product" && category == $category]`;
+// Use groq to define queries
+export const Query = groq`*[_type == "product"]`;
+export const productByIdQuery = groq`*[_type == "product" && _id == $id][0]`;
+export const productsByCategoryQuery = groq`*[_type == "product" && category == $category]`;
 
-export const allProductsQuery = defineQuery(`*[_type == "product"]`)
-// Fetch Products with Pagination (e.g., 10 products per page)
-
-// const query = `*[_type == "product"] | order(_createdAt desc) [0...10]`;
-// Fetch Products by Tag
-
-// const query = `*[_type == "product" && tags match $tag]`;
+export const allProductsQuery = groq`*[_type == "product"]`;

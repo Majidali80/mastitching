a1b3b0866117c2db6ab47509939ec55293772e33
Majid Ali <160537411+Majidali80@users.noreply.github.com>
2025-02-18 20:58:39 +0500

    Update page.tsx

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import Head from 'next/head';
 import { useRouter } from 'next/navigation';
 import { useCart } from '../context/cartContext';

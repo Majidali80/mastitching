18298b1675d12e955a05cb8a0fc442494cd12e01
Majid Ali <160537411+Majidali80@users.noreply.github.com>
2025-02-19 12:37:34 +0500

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,6 +9,11 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
+        myDgold:"#ED9B33",
+			myLgold:"#F1B434",
+			myBkack:"#111011",
+			myLblack:"#101820",
+			myBlue:"#2536be",
         background: "var(--background)",
         foreground: "var(--foreground)",
       },

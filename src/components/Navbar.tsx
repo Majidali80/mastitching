9a6a27bcccf9d2778d309fc4f6ae85a6662fc61b
Majid Ali <160537411+Majidali80@users.noreply.github.com>
2025-02-20 00:27:56 +0500

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -11,34 +11,40 @@ const Navbar = () => {
   const totalItemsInCart = cart.reduce((total, item) => total + item.quantity, 0);
 
   return (
-    <nav className="bg-yellow-500 text-white p-4 flex justify-between items-center">
-      {/* Logo or Brand Name */}
-      <Link href="/" className="text-2xl font-extrabold">
-        <span className="text-white">Shop</span>
+    <nav className="bg-gradient-to-r from-yellow-100 to-orange-500 text-white p-2 flex justify-between items-center">
+    {/* Logo or Brand Name */}
+    <Link href="/" className="flex items-center h-10 ">
+      <img 
+        src="/logo.png" // Replace with the path to your logo image
+        alt="Logo"
+        className="h-20 w-28 mr-2" // Adjust the height of the logo as needed
+      />
+      <span className="text-2xl font-extrabold text-myBkack">MA Tailor</span>
+    </Link>
+  
+    <div className="flex space-x-4 items-center">
+      {/* Wishlist Icon */}
+      <Link href="/wishlist">
+        <button className="relative text-red-500 hover:text-red-600" aria-label="Wishlist">
+          {/* Placeholder icon, adjust according to your design */}
+          <FaHeart size={32} />
+        </button>
       </Link>
-
-      <div className="flex space-x-4 items-center">
-        {/* Wishlist Icon */}
-        <Link href="/wishlist">
-          <button className="relative text-red-500 hover:text-red-600" aria-label="Wishlist">
-            {/* Placeholder icon, adjust according to your design */}
-            <FaHeart size={32} />
-          </button>
-        </Link>
-
-        {/* Cart Icon */}
-        <Link href="/cart">
-          <button className="relative text-blue-500 hover:text-blue-600" aria-label="Cart">
-            <FaShoppingCart size={32} />
-            {totalItemsInCart > 0 && (
-              <span className="absolute top-0 right-0 bg-black-500 text-white text-xs font-bold rounded-full w-5 h-5 flex items-center justify-center">
-                {totalItemsInCart}
-              </span>
-            )}
-          </button>
-        </Link>
-      </div>
-    </nav>
+  
+      {/* Cart Icon */}
+      <Link href="/cart">
+        <button className="relative text-blue-500 hover:text-blue-600" aria-label="Cart">
+          <FaShoppingCart size={32} />
+          {totalItemsInCart > 0 && (
+            <span className="absolute top-0 right-0 bg-black-500 text-white text-xs font-bold rounded-full w-5 h-5 flex items-center justify-center">
+              {totalItemsInCart}
+            </span>
+          )}
+        </button>
+      </Link>
+    </div>
+  </nav>
+  
   );
 };
 

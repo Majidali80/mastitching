f7ed79372b2598c812e6d7e95674e8d308ce8532
Majid Ali <160537411+Majidali80@users.noreply.github.com>
2025-02-21 15:30:39 +0500

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -6,12 +6,12 @@ const Hero = () => {
   return (
     <div className="mb-[100px]">
       <div
-  className="hero min-h-[80vh] custom-img bg-fixed bg-center bg-no-repeat">
+  className="hero min-h-[80vh] custom-img bg-fixed bg-center bg-no-repeat mt-10">
   <div className="hero-overlay bg-opacity-70"></div>
   <div className="hero-content text-neutral-content text-center">
     <div className="max-w-md">
-      <h1 className="mb-5 text-5xl scroll-m-20 font-extrabold tracking-tight lg:text-5xl text-myYellow">ASLAM O ALIKUM</h1>
-      <p className="mb-5 text-myOrange font-bold scroll-m-40 text-xl tracking-tight">
+      <h1 className="mb-5 text-5xl scroll-m-20 font-extrabold tracking-tight lg:text-5xl text-yellow-400">ASLAM O ALIKUM</h1>
+      <p className="mb-5 text-orange-500 font-bold scroll-m-40 text-xl tracking-tight">
       Your Instant Marketplace! Where Deliver At Your Door Step.
       </p>
       <Button className="outline outline-offset-2 outline-1 group hover:rounded-3xl duration-300 hover:outline-myYellow">

62adabfefbafa6c65cf1ee238d0b67871e80be3f
Majid Ali <160537411+Majidali80@users.noreply.github.com>
2025-02-21 19:36:04 +0500

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -7,16 +7,16 @@ const Hero = () => {
     <div className="mb-20">
       <div className="hero min-h-[90vh] custom-img bg-fixed bg-center bg-no-repeat mt-5">
         <div className="hero-overlay bg-opacity-70"></div>
-        <div className="hero-content text-center flex flex-col items-center justify-center">
-          <div className="max-w-md flex flex-col items-center justify-center">
-            <h1 className="mb-5 text-5xl scroll-m-20 font-extrabold tracking-tight lg:text-5xl text-yellow-400">
+        <div className="hero-content flex items-center justify-center min-h-full">
+          <div className="text-center max-w-md">
+            <h1 className=" text-fixed  mt-72 mb-5 text-5xl scroll-m-20 font-extrabold tracking-tight lg:text-5xl text-yellow-400">
               ASLAM O ALIKUM
             </h1>
-            <p className="mb-8 text-orange-500 font-bold scroll-m-40 text-xl tracking-tight text-center">
+            <p className="mt-5 mb-8 text-orange-500 font-bold scroll-m-40 text-xl tracking-tight text-center">
               Your Instant Marketplace! Where Deliver At Your Door Step.
             </p>
-            <Button className="outline outline-offset-2 outline-1 group hover:rounded-3xl duration-300 hover:outline-myYellow w-fit">
-              <HiMiniShoppingBag className="mr-2 group-hover:animate-bounce"/> SHOP NOW
+            <Button className="outline outline-offset-2 outline-1 group hover:rounded-3xl duration-300 hover:outline-yellow-400 w-fit">
+              <HiMiniShoppingBag className="text-myYellow mr-2 group-hover:animate-bounce"/> SHOP NOW
             </Button>
           </div>
         </div>
@@ -25,4 +25,4 @@ const Hero = () => {
   )
 }
 
-export default Hero
+export default Hero;

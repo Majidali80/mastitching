c0b092c4cf60ee914c239f168830f369818131fa
Majid Ali <160537411+Majidali80@users.noreply.github.com>
2025-02-22 00:47:01 +0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,21 @@
-
-import Hero from "@/components/Hero";
-import Products from "@/components/Products";
-
+import ImageCarousel from '@/components/ImageCarousel';
+import Products from '@/components/Products';
 
 export default function Home() {
+  const images = [
+    '/1 (1).jpeg',
+    '/1 (2).jpeg',
+    '/1 (3).jpeg'
+  ];
+
   return (
-    <div>
-<Hero />
-      <Products />
-    </div>
+    <main className="flex min-h-screen flex-col items-center justify-between">
+      <div className="w-full">
+        <ImageCarousel images={images} />
+        <div className="mt-8 sm:mt-12 md:mt-16 lg:mt-20">
+          <Products />
+        </div>
+      </div>
+    </main>
   );
 }

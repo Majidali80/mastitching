be89a6620819b3c704aed8e5332691a935c5b03f
Majid Ali <160537411+Majidali80@users.noreply.github.com>
2025-02-21 08:32:01 +0500

    Update index.ts

--- src/sanity/schemaTypes/index.ts
+++ src/sanity/schemaTypes/index.ts
@@ -1,7 +1,10 @@
 import { type SchemaTypeDefinition } from 'sanity'
 import product from './product'
-
+import order from './order'
+import customer from './customer'
+import orderTracking from './orderTracking'
+import inventory from './inventory'
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [product],
+  types: [product, order, customer, orderTracking, inventory],
 }

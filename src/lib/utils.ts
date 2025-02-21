12a0afc39cfd40db1554052394a744c5e4bc5262
Majid Ali <160537411+Majidali80@users.noreply.github.com>
2025-02-21 12:49:20 +0500

    Update utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,15 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
+import { createClient } from 'next-sanity'
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
-} 
+}
+
+export const client = createClient({
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
+  apiVersion: '2024-03-19', // Use current date
+  useCdn: process.env.NODE_ENV === 'production',
+  token: process.env.SANITY_ACCESS_TOKEN,
+}) 

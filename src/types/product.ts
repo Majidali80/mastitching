0a8a32e19c94a76ed83408d0e4d6b524f93554d4
Majid Ali <160537411+Majidali80@users.noreply.github.com>
2025-02-23 21:05:30 +0500

    Update product.ts

--- src/types/product.ts
+++ src/types/product.ts
@@ -1,3 +1,31 @@
+// types.ts
+
 export interface Product {
-  selectedSize?: string;
-} 
+  _id: string;
+  _type: 'product';
+  title: string;
+  description: string;
+  fabricType: string;
+  materials: string[];
+  dimensions: string;
+  price: number;
+  images: { _type: 'image'; asset: { _ref: string; _type: 'reference' } }[];
+  tags: string[];
+  category: string;
+  inventory: number;
+  colors: string[];
+  discountPercentage: number;
+  careInstructions: string;
+  availability: string;
+  customerReviews: string[];
+  dateAdded: string;
+  shippingInformation: string;
+  specialOffers: string;
+  slug: { _type: 'slug'; current: string };
+  reviews: { _type: 'review'; reviewText: string; rating: number }[];
+  stockQuantity: number;
+  isNewArrival: boolean;
+  isBestSeller: boolean;
+  productName: string;
+}
+

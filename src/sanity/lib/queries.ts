cae4fc459d2d7a57cef6dae8ca05343a768f7708
Majid Ali <160537411+Majidali80@users.noreply.github.com>
2025-02-24 02:38:58 +0500

    Update queries.ts

--- src/sanity/lib/queries.ts
+++ src/sanity/lib/queries.ts
@@ -4,7 +4,8 @@ export const productQuery = groq`*[_type == "product"]`;
 
 export const productByIdQuery = groq`*[_type == "product" && _id == $id][0]`;
 
-export const stitchedProductQuery = groq`
- *[_type == "stitching"]`;
+export const stitchedProductsQuery = groq`*[_type == "stitching"]`;
 
 
+
+export const stitchedProductByIdQuery = groq`*[_type == "stitching" && _id == $id][0]`;

991a3979b7c3cf6afea2bc26b4dd174c5f82573f
Majid Ali <160537411+Majidali80@users.noreply.github.com>
2025-02-23 22:59:44 +0500

    Update structure.ts

--- src/sanity/structure.ts
+++ src/sanity/structure.ts
@@ -1,7 +1,12 @@
-import type {StructureResolver} from 'sanity/structure'
+import type { StructureResolver, StructureBuilder } from '@sanity/structure'
 
 // https://www.sanity.io/docs/structure-builder-cheat-sheet
-export const structure: StructureResolver = (S) =>
+export const structure: StructureResolver = (S: typeof StructureBuilder) =>
   S.list()
     .title('Content')
     .items(S.documentTypeListItems())
+
+declare module '@sanity/structure' {
+  export type StructureResolver = (S: typeof StructureBuilder) => unknown;
+  // Add other types as needed
+}

c5750111dfe47882238e8eac4a8865c49ec150f2
Majid Ali <160537411+Majidali80@users.noreply.github.com>
2025-02-23 22:59:48 +0500

    Update sanity.config.ts

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,12 +1,12 @@
 'use client'
 
 /**
- * This configuration is used to for the Sanity Studio that’s mounted on the `\src\app\studio\[[...tool]]\page.tsx` route
+ * This configuration is used to for the Sanity Studio that's mounted on the `\src\app\studio\[[...tool]]\page.tsx` route
  */
 
 import {visionTool} from '@sanity/vision'
 import {defineConfig} from 'sanity'
-import {structureTool} from 'sanity/structure'
+import {structureTool} from '@sanity/structure'
 
 // Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
 import {apiVersion, dataset, projectId} from './src/sanity/env'

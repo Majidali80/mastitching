5dec2c5f4b8fc10311eb772aa0d175f28b34cea9
Majid Ali <160537411+Majidali80@users.noreply.github.com>
2025-02-23 21:04:36 +0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
 import ImageCarousel from '@/components/ImageCarousel';
 import Products from '@/components/Products';
 import FeaturedCategories from '@/components/FeaturedCategories';
-import UserProfile from '../components/UserProfile';
+import Stitching from '@/components/stitching';
+
 
 export default function Home() {
   const images = [
@@ -16,7 +17,10 @@ export default function Home() {
         <ImageCarousel images={images} />
         <div className="mt-8 sm:mt-12 md:mt-16 lg:mt-20">
           <Products />
+          <Stitching />
+          
           <FeaturedCategories />
+         
         </div>
       </div>
     </main>

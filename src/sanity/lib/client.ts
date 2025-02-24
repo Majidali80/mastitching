2d6df7896b63df5c2dcaf8f5939cf290111cf35b
Majid Ali <160537411+Majidali80@users.noreply.github.com>
2025-02-24 10:59:58 +0500

    Update client.ts

--- src/sanity/lib/client.ts
+++ src/sanity/lib/client.ts
@@ -1,18 +1,24 @@
-import { createClient, createImageUrlBuilder } from 'next-sanity'
+import { createClient } from 'next-sanity'
+import createImageUrlBuilder from '@sanity/image-url'
+
  
 
 import { apiVersion, dataset, projectId } from '../env'
 
-const client = createClient({
+const config = {
   projectId,
   dataset,
   apiVersion,
   useCdn: true, // Set to false if statically generating pages, using ISR or tag-based revalidation
-})
+}
+
+const client = createClient(config)
 
 // Create a URL builder for images
 const urlFor = (source: string | { _type: 'image'; asset: { _ref: string } }) => {
-  return createImageUrlBuilder(client).image(source); // Use the image builder to generate the URL
+  return createImageUrlBuilder(client).image(source); //  Use the image builder to generate the URL
 }
 
 export { client, urlFor }
+
+export default client;

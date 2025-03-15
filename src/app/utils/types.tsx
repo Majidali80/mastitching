7c2719fe3b0ece0611657de30c682ee6a21e2738
Majid Ali <160537411+Majidali80@users.noreply.github.com>
2025-03-15 11:37:10 +0500

    Update types.tsx

--- src/app/utils/types.tsx
+++ src/app/utils/types.tsx
@@ -34,8 +34,8 @@ export interface Product {
   reviews: { rating: number; comment: string }[];
   productImage?: string;
   // Adding optional fields for the cart context
-  selectedSize: string; // This is added to store selected size
-  quantity: number;     // This is added to store quantity
+  selectedSize?: string; // This is added to store selected size
+  quantity?: number;     // This is added to store quantity
   
 }
 
@@ -51,3 +51,34 @@ export type cart = {
   discountPercentage: number;
   discount?: (price: number, discount: number) => number; // This is optional and can be a function to apply discount
 };
+
+export interface Perfume {
+  _id: string;
+  title: string;
+  slug: {
+    current: string;
+  };
+  description?: string;
+  price: number;
+  sizes?: { size: string; price: number }[];
+  image: { asset: { url: string } }; 
+  tags?: string[];
+  category?: "floral" | "woody" | "oriental" | "fresh";
+  inventory: number;
+  colors?: string[];
+  discountPercentage?: number;
+  availability: string;
+  customerReviews?: string[];
+  dateAdded?: string;
+  shippingInformation?: string;
+  specialOffers?: string;
+  scentNotes?: {
+    top: string[];
+    heart: string[];
+    base: string[];
+  };
+  bestSeller?: boolean;
+  // For cart context
+  selectedSize?: string;
+  quantity?: number;
+}

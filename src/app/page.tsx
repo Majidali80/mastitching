dd92d5027c35dc94a195d32e267eed49fd81cea9
Majid Ali <160537411+Majidali80@users.noreply.github.com>
2025-03-15 11:37:00 +0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,17 @@
 
 import Products from "@/components/Products";
+import Perfumes from "@/components/Perfume";
+import Hero from"@/components/Hero";
+import Footer from "@/components/Footer";
 
 
 export default function Home() {
   return (
     <div>
-
+<Hero />
       <Products />
+      <Perfumes />
+      <Footer />
     </div>
   );
 }
